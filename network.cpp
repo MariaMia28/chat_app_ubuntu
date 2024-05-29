82beb88d6aa7ac25a312df3ff6b1fa288b7c486a
MariaMia28 <maria.mihaita03@e-uvt.ro>
2024-05-29 05:26:26 +0300

    socket file descriptor + server address structure

--- network.cpp
+++ network.cpp
@@ -10,3 +10,22 @@
 
 #define PORT 12345
 
+using namespace std;
+
+int createServerSocket(){
+    int serverFd;
+    struct sockaddr _in serverAddr;
+
+    //creating socket file descriptor
+    if ((serverFd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    //setting up the server address structure
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY; //listen on all available interfaces
+    serverAddr.sin_port = htons(PORT);
+}
+
