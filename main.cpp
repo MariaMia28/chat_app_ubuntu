fcb3b3a5f44aa8309a0d1dfd7826ec16b97c43d7
MariaMia28 <maria.mihaita03@e-uvt.ro>
2024-05-29 05:13:03 +0300

    basic variable structure and steps of execution added

--- main.cpp
+++ main.cpp
@@ -12,3 +12,40 @@ struct Client {
     int socketFd;
 
 };
+
+int main(int argc, char* argv[]) {
+    if (argc < 2)
+    {
+        string serverAddress = getServerAddressFromUser(); //implement it in ui.cpp
+        int clientSocket = connectToServer(serverAddress); //implement it in network.cpp
+
+        thread receiveThread(receiveMessages, clientSocket);
+
+        while(true) {
+            string message = getMessageFromUser(); //implement it in ui.cpp
+            sendMessage(clientSocket, message); //implement in network.cpp
+        }
+    } else if(strcmp(argv[1], "--server") == 0) {
+        //server mode
+        int serverSocket = createServerSocket(); // implement in network.cpp
+
+        while (true)
+        {
+            int clientSocket = acceptClientConnection(serverSocket); //implement in network.cpp
+            string clientName = getClientConnection(serverSocket);
+
+            //storing client information
+            clients.push_back({clientName, clientSocket});
+
+            thread clientThread(handleClient, clientSocket);
+            clientThread.detach(); //let the thread run independently
+
+        }
+        
+    } else {
+        cout << "Invalid argument. Use --server to run as a server." << endl;
+        return 1;
+    }
+    
+    return 0;
+}
